b81cc2f7367d8f789d9caff4023f628a86796f8f
BeulaEvangelin <nicholbe@sheridancollege.ca>
2023-09-21 09:06:37 -0400

    a1_part1_Data Structure

---
+++ a1.js
@@ -0,0 +1,26 @@
+let hospital = {
+    hospitalName:"Harmony Children's Hospital",
+    patients:[
+    {
+        id:"001",
+        fullName:"John Hart",
+        dateOfBirth:"07-11-2014",
+        symptoms:["Sore Throat", "Headache", "Vomiting"]
+    },
+
+    {
+        id:"002",
+        fullName:"Belle Finnegan",
+        dateOfBirth:"01-23-2012",
+        symptoms:["Redness in the eye", "Itchiness", "Light sensitivity"]
+    },
+   
+    {
+        id:"003",
+        fullName:"Robin Adams",
+        dateOfBirth:"12-06-2015",
+        symptoms:["Fever", "Vomiting", "Stomach pain"]
+
+    }
+]
+}
