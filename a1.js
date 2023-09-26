b04a7799ee95b8d53ae370965124aa32c8ff2d06
BeulaEvangelin <nicholbe@sheridancollege.ca>
2023-09-26 10:37:41 -0400

    tags added

--- a1.js
+++ a1.js
@@ -1,25 +1,25 @@
 let hospital = {
-    hospitalName:"Harmony Children's Hospital",
+    hospitalName:"<h1>Harmony Children's Hospital</h1>",
     patients:[
     {
         id:"001",
-        fullName:"John Hart",
-        dateOfBirth:"07-11-2014",
-        symptoms:["Sore Throat", "Headache", "Vomiting"]
+        fullName:"<h2>John Hart, ",
+        dateOfBirth:"07-11-2014</h2>",
+        symptoms:["<li>Sore Throat</li>", "<li>Headache</li>", "<li>Vomiting</li>"]
     },
 
     {
         id:"002",
-        fullName:"Belle Finnegan",
-        dateOfBirth:"01-23-2012",
-        symptoms:["Redness in the eye", "Itchiness", "Light sensitivity"]
+        fullName:"<h2>Belle Finnegan, ",
+        dateOfBirth:"01-23-2012</h2>",
+        symptoms:["<li>Redness in the eye</li>", "<li>Itchiness</li>", "<li>Light sensitivity</li>"]
     },
    
     {
         id:"003",
-        fullName:"Robin Adams",
-        dateOfBirth:"12-06-2015",
-        symptoms:["Fever", "Vomiting", "Stomach pain"]
+        fullName:"<h2>Robin Adams,",
+        dateOfBirth:"12-06-2015</h2>",
+        symptoms:["<li>Fever</li>", "<li>Vomiting</li>", "<li>Stomach pain</li>"]
 
     }
 ]
@@ -36,9 +36,9 @@ for(let i=0; i<hospital.patients.length; i++){
 showPatients();
 
 function getPatients(pts){
-    let randomNumber = Math.floor(Math.random()*4);
+    let randomNumber = Math.floor(Math.random()*pts.length);
     let pt = pts[randomNumber];
     return pt.id;
   }
-    console.log(); 
-    getPatients(hospital.patients);
+    console.log(getPatients(hospital.patients)); 
+
